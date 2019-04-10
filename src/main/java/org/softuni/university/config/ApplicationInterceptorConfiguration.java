153ce2e7333030925b4c2312f9197abf6a21275e
DimovDimo <d.dimov1996@gmail.com>
2019-04-10 12:27:43 +0300

    added ImageInterceptor in UniversityApplication

--- src/main/java/org/softuni/university/config/ApplicationInterceptorConfiguration.java
+++ src/main/java/org/softuni/university/config/ApplicationInterceptorConfiguration.java
@@ -1,6 +1,8 @@
 package org.softuni.university.config;
 
-import org.softuni.university.web.interceptors.IconTitleInterceptor;
+import org.softuni.university.web.interceptors.ImageInterceptor;
+import org.softuni.university.web.interceptors.IconInterceptor;
+import org.softuni.university.web.interceptors.TitleInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,15 +10,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ApplicationInterceptorConfiguration implements WebMvcConfigurer {
 
-    private final IconTitleInterceptor iconTitleInterceptor;
+    private final IconInterceptor iconInterceptor;
+    private final TitleInterceptor titleInterceptor;
+    private final ImageInterceptor imageInterceptor;
 
-    public ApplicationInterceptorConfiguration(IconTitleInterceptor iconTitleInterceptor) {
-        this.iconTitleInterceptor = iconTitleInterceptor;
+    public ApplicationInterceptorConfiguration(IconInterceptor iconInterceptor, TitleInterceptor titleInterceptor, ImageInterceptor imageInterceptor) {
+        this.iconInterceptor = iconInterceptor;
+        this.titleInterceptor = titleInterceptor;
+        this.imageInterceptor = imageInterceptor;
     }
 
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(this.iconTitleInterceptor);
+        registry.addInterceptor(this.iconInterceptor);
+        registry.addInterceptor(this.titleInterceptor);
+        registry.addInterceptor(this.imageInterceptor);
     }
 }
---
+++ src/main/java/org/softuni/university/web/interceptors/TitleInterceptor.java
@@ -0,0 +1,32 @@
+package org.softuni.university.web.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class TitleInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (modelAndView == null) {
+            modelAndView = new ModelAndView();
+        }
+
+        String title = getTitle(modelAndView);
+
+        modelAndView.addObject("title", title);
+    }
+
+    private String getTitle(ModelAndView modelAndView) {
+        String title = modelAndView.getViewName();
+        title = title.substring(0, 1).toUpperCase() + title.substring(1);
+        title = title.replace("-", " ");
+        title = title.replace("/", " ");
+
+        return title;
+    }
+}
