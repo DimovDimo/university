0de5697dc925b9a66ea62d9ba52d2f0a29aa36b4
DimovDimo <d.dimov1996@gmail.com>
2019-04-10 10:44:15 +0300

    added sec:authentication="name" in UniversityApplication

--- src/main/java/org/softuni/university/config/ApplicationInterceptorConfiguration.java
+++ src/main/java/org/softuni/university/config/ApplicationInterceptorConfiguration.java
@@ -1,7 +1,6 @@
 package org.softuni.university.config;
 
 import org.softuni.university.web.interceptors.IconTitleInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +10,10 @@ public class ApplicationInterceptorConfiguration implements WebMvcConfigurer {
 
     private final IconTitleInterceptor iconTitleInterceptor;
 
-    @Autowired
     public ApplicationInterceptorConfiguration(IconTitleInterceptor iconTitleInterceptor) {
         this.iconTitleInterceptor = iconTitleInterceptor;
     }
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/org/softuni/university/web/interceptors/IconTitleInterceptor.java
+++ src/main/java/org/softuni/university/web/interceptors/IconTitleInterceptor.java
@@ -12,14 +12,23 @@ public class IconTitleInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        String iconLink = "https://res.cloudinary.com/dimo-cloud/image/upload/v1554833059/icon.png";
-        String title = "University";
-
         if (modelAndView == null) {
             modelAndView = new ModelAndView();
         }
 
+        String iconLink = "https://res.cloudinary.com/dimo-cloud/image/upload/v1554834927/icon2.png";
+        String title = getTitle(modelAndView);
+
         modelAndView.addObject("iconLink", iconLink);
         modelAndView.addObject("title", title);
+    }
+
+    private String getTitle(ModelAndView modelAndView) {
+        String title = modelAndView.getViewName();
+        title = title.substring(0, 1).toUpperCase() + title.substring(1);
+        title = title.replace("-", " ");
+        title = title.replace("/", " ");
+
+        return title;
     }
 }
